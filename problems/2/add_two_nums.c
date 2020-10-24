b8ece93edb76640a6e20833c29285b469e668a75
Roman Sofyin <roman.sofyin@dell.com>
2020-10-24 10:03:09 +0300

    Pass problem 2

--- problems/2/add_two_nums.c
+++ problems/2/add_two_nums.c
@@ -7,21 +7,10 @@
  */
 
 /*
-    1 2 3 4 5 6     =       654321
-    5 6 7 8         =         8765
-    
-    6 8 0 3 6 6     =       663086
-    
-    Wrong Answer, Details 
-    Input
-    [9,9,9,9,9,9,9]
-    [9,9,9,9]
-    Output
-    [8,9,9,9,10,9]
-    Expected
-    [8,9,9,9,0,0,0,1]
+    [1,2,3,4,5,6] = 654321
+    [5,6,7,8]     =   8765    
+    [6,8,0,3,6,6] = 663086
 */
-
 
 #define MAX_RESULT_LEN (200+1)
 
@@ -33,28 +22,31 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     int n = 0;
     
     while ( l1 && l2 ) {
-        l3->val = (l1->val + l2->val + n) % 10;
-        n = (int) ( (l1->val + l2->val) / 10 );
+        l3->val =         (l1->val + l2->val + n) % 10;
+        n       = (int) ( (l1->val + l2->val + n) / 10 );
         l1 = l1->next;
         l2 = l2->next;
         (l3++)->next = (l1 || l2 || n) ? (l3 + 1) : NULL;
     }
     
-    if (!l1 && !l2) {
+    if (!l1 && !l2 && n) { /* 'n' is what is left to add up */
         l3->val = n;
-    } else {
+        l3->next = NULL;
+    } else if (l1 || l2) { /* we have either l1 or l2 non-NULL */
         struct ListNode* l = l1 ? l1 : l2;
-        l3->val = l->val + n;
-        (l3++)->next = l3 + 1;
-        l = l->next;
         
         while (l) {
-            l3->val = l->val;
-            (l3++)->next = (l->next) ? (l3+1) : NULL;
+            l3->val = (l->val + n) % 10;
+            n = (int) (l->val + n) / 10;
             l = l->next;
+            (l3++)->next = (l || n) ? (l3 + 1) : NULL;
         }
+
+        if (n) { /* 'l' is exceeded but 'n' needs to be added up */
+            l3->val = n;
+            l3->next = NULL;
+        } 
     }
     
     return resultList;
 }
-
