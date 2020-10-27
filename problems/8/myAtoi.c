dbe4f0bf19649d485a01c38d9b6b31dd7c5c2c03
Roman Sofyin <roman.sofyin@dell.com>
2020-10-27 09:37:28 +0300

    Still woring on problem 8

--- problems/8/myAtoi.c
+++ problems/8/myAtoi.c
@@ -1,34 +1,53 @@
 int myAtoi(char * s){
     int res;
-    bool pos = 1;
+    int pos = 1;
     printf("100: INT_MIN=%d, INT_MAX=%d \n", INT_MIN, INT_MAX);   
-    
-   s = "42"; // "   +123e34";
     
     while (*s == ' ')
         s++;
     
+    printf("350: *s=%c \n", *s);
+    
     if (*s != '+' && *s != '-' && !isdigit(*s))
         return 0;
     
-    if (*s == '+')
-        s++;
-    else if (*s == '-') {
-        pos = -1;
+    if (*s == '+' || *s == '-') {
+        if ( !isdigit(*(s+1)) )
+            return 0;
+        if (*s == '-')
+            pos = -1;
         s++;
     }
     
+    int int_max_div_10 = INT_MAX / 10;
+    int int_min_div_10 = INT_MIN / 10;
+
+    
     char cz = '0';
-    res = (*(s++) - cz) * pos;
+    res = (*s - cz) * pos;
     
-    printf("400: res=%d \n", res);
+    printf("400: res=%d, pos=%d \n", res, pos);
     
-    while (isdigit(*(s++))) {
-        int acc = res * 10 + (*s - cz) * pos;
+    int acc;
+    
+    while (isdigit(*(++s))) {
+        if (pos == 1) {
+            if ( int_max_div_10 >= res )
+                acc = res * 10;
+        } else {
+            
+        }
+        acc = res * 10;
+        
+        printf("480: acc=%d, res=%d \n", acc, res);
+        acc = acc + (*s - cz) * pos;
+        printf("500: acc=%d, res=%d \n", acc, res);
+        
         if (pos == 1 && acc < res)
             return INT_MAX;
         if (pos == -1 && acc > res)
             return INT_MIN;
+        
         res = acc;
     }
     
