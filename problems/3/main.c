b03e4357587272764a2d2eedc7be925cb90f1296
Roman Sofin <roma.sofin@gmail.com>
2020-11-08 20:50:25 +0300

    Add TEST_RUN macro

--- problems/3/main.c
+++ problems/3/main.c
@@ -1,7 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int lengthOfLongestSubstring(char*);
+
+#define TEST_RUN(str, len) \
+{ \
+    char* __str = str ? str : "NULL"; \
+    int __len = 0; \
+    if ( ( (__len) = lengthOfLongestSubstring(str)) != (len) ) \
+        printf( "test failed: str=%s, expected=%d, returned=%d\n", __str, (len), (__len) ); \
+}
 
 int main () {
 
-    lengthOfLongestSubstring("abcadefg");
+    TEST_RUN("abcadefg", 3);
+    TEST_RUN("", 0);
+    TEST_RUN(NULL, 0);
     
     return 0;
 }
