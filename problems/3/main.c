2cfb68ca67acddcc5344fbe7ff3080a38d66c92b
Roman Sofin <roma.sofin@gmail.com>
2020-11-09 12:08:59 +0300

    Implement basic algorithm

--- problems/3/main.c
+++ problems/3/main.c
@@ -13,15 +13,32 @@ int lengthOfLongestSubstring(char*);
 
 int main () {
 
-    TEST_RUN("abcadefg", 3);
-    TEST_RUN("", 0);
-    TEST_RUN(NULL, 0);
+    TEST_RUN("abcadefg", 3);    // "abc"
+    TEST_RUN("aabcdefg", 1);    // "a" 
+    TEST_RUN("abcdefg", 7);     // "abcdefg"
+    TEST_RUN("pwwkew", 7);      // "wke"
+    TEST_RUN("", 0);            // ""
+    TEST_RUN(NULL, 0);          // excessive, not the task case
     
     return 0;
 }
 
 
-int lengthOfLongestSubstring(char * s){
+int lengthOfLongestSubstring(char * s)
+{
+    if (!s)
+        return 0;
+    
+    char* si = s;
 
-    return 0;
+    while (*si) {
+        for (char* sj = s; sj < si; sj++) {
+            if ( *sj == *si )
+                return si-s;
+        }
+        si++;
+    }
+
+    /* whole 's' string contains unique chars */
+    return si-s;
 }
